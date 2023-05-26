d1c4ae93ed30f1d626068f401d939225923567e9
Torben Soennecken <torben.soennecken@adesso.de>
2023-05-26 11:45:57 +0200

    [-] Removed InModuleScope where not required

--- Tests/ShouldAssertMAC.Tests.ps1
+++ Tests/ShouldAssertMAC.Tests.ps1
@@ -3,85 +3,83 @@ BeforeAll {
     Import-Module $manifestPath -Force -ErrorAction Stop
 }
 
-InModuleScope PSDeviceVerification {
-    Describe "Assert-DeviceInformation -MAC tests" {
+Describe "Assert-DeviceInformation -MAC tests" {
 
-        Context "Valid MAC addresses" {
+    Context "Valid MAC addresses" {
     
-            It "Returns valid for 00-11-22-33-44-55" {
-                $result = Assert-DeviceInformation -MAC "00-11-22-33-44-55"
-                $result["MAC"]["Valid"] | Should -Be $true
-                $result["MAC"]["Data"] | Should -Be "00:11:22:33:44:55"
-            }
-    
-            It "Returns valid for 00:11:22:33:44:55" {
-                $result = Assert-DeviceInformation -MAC "00:11:22:33:44:55"
-                $result["MAC"]["Valid"] | Should -Be $true
-                $result["MAC"]["Data"] | Should -Be "00:11:22:33:44:55"
-            }
-    
-            It "Returns valid for 001122334455" {
-                $result = Assert-DeviceInformation -MAC "001122334455"
-                $result["MAC"]["Valid"] | Should -Be $true
-                $result["MAC"]["Data"] | Should -Be "00:11:22:33:44:55"
-            }
-    
-            It "Returns valid for ec:28:d3:a5:13:19" {
-                $result = Assert-DeviceInformation -MAC "ec:28:d3:a5:13:19"
-                $result["MAC"]["Valid"] | Should -Be $true
-                $result["MAC"]["Data"] | Should -Be "EC:28:D3:A5:13:19"
-            }
-
-            It "Returns valid for F8:5B:6E:B4:C9:F4" {
-                $result = Assert-DeviceInformation -MAC "F8:5B:6E:B4:C9:F4"
-                $result["MAC"]["Valid"] | Should -Be $true
-                $result["MAC"]["Data"] | Should -Be "F8:5B:6E:B4:C9:F4"
-            }
+        It "Returns valid for 00-11-22-33-44-55" {
+            $result = Assert-DeviceInformation -MAC "00-11-22-33-44-55"
+            $result["MAC"]["Valid"] | Should -Be $true
+            $result["MAC"]["Data"] | Should -Be "00:11:22:33:44:55"
         }
     
-        Context "Invalid MAC addresses" {
+        It "Returns valid for 00:11:22:33:44:55" {
+            $result = Assert-DeviceInformation -MAC "00:11:22:33:44:55"
+            $result["MAC"]["Valid"] | Should -Be $true
+            $result["MAC"]["Data"] | Should -Be "00:11:22:33:44:55"
+        }
     
-            It "Returns invalid for 00-11-22-33-44-5" {
-                $result = Assert-DeviceInformation -MAC "00-11-22-33-44-5"
-                $result["MAC"]["Valid"] | Should -Be $false
-                $result["MAC"]["Reason"] | Should -Be "MAC address is not 12 characters long"
-            }
+        It "Returns valid for 001122334455" {
+            $result = Assert-DeviceInformation -MAC "001122334455"
+            $result["MAC"]["Valid"] | Should -Be $true
+            $result["MAC"]["Data"] | Should -Be "00:11:22:33:44:55"
+        }
     
-            It "Returns invalid for 00-11-22-33-44-556" {
-                $result = Assert-DeviceInformation -MAC "00-11-22-33-44-556"
-                $result["MAC"]["Valid"] | Should -Be $false
-                $result["MAC"]["Reason"] | Should -Be "MAC address is not 12 characters long"
-            }
+        It "Returns valid for ec:28:d3:a5:13:19" {
+            $result = Assert-DeviceInformation -MAC "ec:28:d3:a5:13:19"
+            $result["MAC"]["Valid"] | Should -Be $true
+            $result["MAC"]["Data"] | Should -Be "EC:28:D3:A5:13:19"
+        }
+
+        It "Returns valid for F8:5B:6E:B4:C9:F4" {
+            $result = Assert-DeviceInformation -MAC "F8:5B:6E:B4:C9:F4"
+            $result["MAC"]["Valid"] | Should -Be $true
+            $result["MAC"]["Data"] | Should -Be "F8:5B:6E:B4:C9:F4"
+        }
+    }
     
-            It "Returns invalid for 00-11-22-33-44-5g" {
-                $result = Assert-DeviceInformation -MAC "00-11-22-33-44-5g"
-                $result["MAC"]["Valid"] | Should -Be $false
-                $result["MAC"]["Reason"] | Should -Be "MAC address contains non-hexadecimal characters"
-            }
+    Context "Invalid MAC addresses" {
     
-            It "Returns invalid for ec:2x:d3:a5:13:19" {
-                $result = Assert-DeviceInformation -MAC "ec:2x:d3:a5:13:19"
-                $result["MAC"]["Valid"] | Should -Be $false
-                $result["MAC"]["Reason"] | Should -Be "MAC address contains non-hexadecimal characters"
-            }
+        It "Returns invalid for 00-11-22-33-44-5" {
+            $result = Assert-DeviceInformation -MAC "00-11-22-33-44-5"
+            $result["MAC"]["Valid"] | Should -Be $false
+            $result["MAC"]["Reason"] | Should -Be "MAC address is not 12 characters long"
+        }
     
-            It "Returns invalid for 01-11-22-33-44-55" {
-                $result = Assert-DeviceInformation -MAC "01-11-22-33-44-55"
-                $result["MAC"]["Valid"] | Should -Be $false
-                $result["MAC"]["Reason"] | Should -Be "MAC address is a multicast address"
-            }
+        It "Returns invalid for 00-11-22-33-44-556" {
+            $result = Assert-DeviceInformation -MAC "00-11-22-33-44-556"
+            $result["MAC"]["Valid"] | Should -Be $false
+            $result["MAC"]["Reason"] | Should -Be "MAC address is not 12 characters long"
+        }
     
-            It "Returns invalid for 03-11-22-33-44-55" {
-                $result = Assert-DeviceInformation -MAC "03-11-22-33-44-55"
-                $result["MAC"]["Valid"] | Should -Be $false
-                $result["MAC"]["Reason"] | Should -Be "MAC address is a multicast address"
-            }
+        It "Returns invalid for 00-11-22-33-44-5g" {
+            $result = Assert-DeviceInformation -MAC "00-11-22-33-44-5g"
+            $result["MAC"]["Valid"] | Should -Be $false
+            $result["MAC"]["Reason"] | Should -Be "MAC address contains non-hexadecimal characters"
+        }
     
-            It "Returns invalid for 02-aa-bb-cc-dd-ee" {
-                $result = Assert-DeviceInformation -MAC "02-aa-bb-cc-dd-ee"
-                $result["MAC"]["Valid"] | Should -Be $false
-                $result["MAC"]["Reason"] | Should -Be "MAC address is not a globally administered address"
-            }
+        It "Returns invalid for ec:2x:d3:a5:13:19" {
+            $result = Assert-DeviceInformation -MAC "ec:2x:d3:a5:13:19"
+            $result["MAC"]["Valid"] | Should -Be $false
+            $result["MAC"]["Reason"] | Should -Be "MAC address contains non-hexadecimal characters"
+        }
+    
+        It "Returns invalid for 01-11-22-33-44-55" {
+            $result = Assert-DeviceInformation -MAC "01-11-22-33-44-55"
+            $result["MAC"]["Valid"] | Should -Be $false
+            $result["MAC"]["Reason"] | Should -Be "MAC address is a multicast address"
+        }
+    
+        It "Returns invalid for 03-11-22-33-44-55" {
+            $result = Assert-DeviceInformation -MAC "03-11-22-33-44-55"
+            $result["MAC"]["Valid"] | Should -Be $false
+            $result["MAC"]["Reason"] | Should -Be "MAC address is a multicast address"
+        }
+    
+        It "Returns invalid for 02-aa-bb-cc-dd-ee" {
+            $result = Assert-DeviceInformation -MAC "02-aa-bb-cc-dd-ee"
+            $result["MAC"]["Valid"] | Should -Be $false
+            $result["MAC"]["Reason"] | Should -Be "MAC address is not a globally administered address"
         }
     }
 }
--- Tests/ShouldPerformLuhnCheck.Tests.ps1
+++ Tests/ShouldPerformLuhnCheck.Tests.ps1
@@ -3,69 +3,91 @@ BeforeAll {
     Import-Module $manifestPath -Force -ErrorAction Stop
 }
 
-InModuleScope PSDeviceVerification {
-    Describe "Test-LuhnCheck" {
-        Context "Valid Data" {
-            It "Returns true for a valid credit card number with an even number of digits" {
+Describe "Test-LuhnCheck" {
+    Context "Valid Data" {
+        It "Returns true for a valid credit card number with an even number of digits" {
+            InModuleScope PSDeviceVerification {
                 $result = Test-LuhnCheck "453275631450418"
                 $result | Should -Be $true
             }
+        }
       
-            It "Returns true for a valid credit card number with an odd number of digits" {
+        It "Returns true for a valid credit card number with an odd number of digits" {
+            InModuleScope PSDeviceVerification {
                 $result = Test-LuhnCheck "492971260134447"
                 $result | Should -Be $true
             }
+        }
       
-            It "Returns true for a valid credit card number with spaces" {
+        It "Returns true for a valid credit card number with spaces" {
+            InModuleScope PSDeviceVerification {
                 $result = Test-LuhnCheck "4929 7126 0134 4443"
                 $result | Should -Be $true
             }
+        }
       
-            It "Returns true for a valid credit card number with hyphens" {
+        It "Returns true for a valid credit card number with hyphens" {
+            InModuleScope PSDeviceVerification {
                 $result = Test-LuhnCheck "4929-7126-0134-4443"
                 $result | Should -Be $true
             }
+        }
       
-            It "Returns true for a valid IMEI number" {
+        It "Returns true for a valid IMEI number" {
+            InModuleScope PSDeviceVerification {
                 $result = Test-LuhnCheck "354715685169067"
                 $result | Should -Be $true
             }
         }
+    }
       
-        Context "Invalid Data" {
-            It "Returns false for an empty input" {
+    Context "Invalid Data" {
+        It "Returns false for an empty input" {
+            InModuleScope PSDeviceVerification {
                 $result = Test-LuhnCheck ""
-                $result | Should -Be $false
-            }
-      
-            It "Returns false for a non-numeric input" {
-                $result = Test-LuhnCheck "4929-7126-abcd-4442"
-                $result | Should -Be $false
-            }
-      
-            It "Returns false for a credit card number with an invalid checksum" {
-                $result = Test-LuhnCheck "492971260134443"
-                $result | Should -Be $false
-            }
-      
-            It "Returns false for a credit card number with an invalid length" {
-                $result = Test-LuhnCheck "492971260134"
                 $result | Should -Be $false
             }
         }
       
-        Context "Edge Cases" {
-            It "Returns false for a credit card number with all the same digits" {
+        It "Returns false for a non-numeric input" {
+            InModuleScope PSDeviceVerification {
+                $result = Test-LuhnCheck "4929-7126-abcd-4442"
+                $result | Should -Be $false
+            }
+        }
+      
+        It "Returns false for a credit card number with an invalid checksum" {
+            InModuleScope PSDeviceVerification {
+                $result = Test-LuhnCheck "492971260134443"
+                $result | Should -Be $false
+            }
+        }
+      
+        It "Returns false for a credit card number with an invalid length" {
+            InModuleScope PSDeviceVerification {
+                $result = Test-LuhnCheck "492971260134"
+                $result | Should -Be $false
+            }
+        }
+    }
+      
+    Context "Edge Cases" {
+        It "Returns false for a credit card number with all the same digits" {
+            InModuleScope PSDeviceVerification {
                 $result = Test-LuhnCheck "4444444444444444"
                 $result | Should -Be $false
             }
+        }
       
-            It "Returns false for a credit card number with alternating digits" {
+        It "Returns false for a credit card number with alternating digits" {
+            InModuleScope PSDeviceVerification {
                 $result = Test-LuhnCheck "1010101010101010"
                 $result | Should -Be $false
             }
+        }
       
-            It "Returns true for a credit card number with the minimum valid length" {
+        It "Returns true for a credit card number with the minimum valid length" {
+            InModuleScope PSDeviceVerification {
                 $result = Test-LuhnCheck "0"
                 $result | Should -Be $true
             }
